47243d2ceef55907f6ae86a8384ae3164f74af5b
Marcondes Ferreira <bshookss@gmail.com>
2023-04-28 21:40:54 -0300

    s

--- src/shared/components/Tecs/Tec/style.ts
+++ src/shared/components/Tecs/Tec/style.ts
@@ -6,9 +6,9 @@ export const Container = styled.div`
   justify-content:center;
   flex-direction: column;
   align-items: center;
-  width:1.8rem;
-  height: 2rem;
-  margin: 0.4rem 0.4rem 0.8rem;
+  width:2.2rem;
+  height:2.2rem;
+  margin: 0.5rem 0.5rem 0.8rem;
   &:hover {
     opacity:0.6;
     cursor: pointer;
