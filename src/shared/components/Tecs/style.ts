e42cbf20e7f38c2835712b02741f3e1813ef1259
Marcondes Ferreira <bshookss@gmail.com>
2023-05-01 20:05:32 -0300

    margin top de icons tec

--- src/shared/components/Tecs/style.ts
+++ src/shared/components/Tecs/style.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
   justify-content:center;
   flex-wrap: wrap;
   text-align: center;
-  margin-top: 1.4rem;
+  margin-top: 1.6rem;
   h2 {
     margin: 1rem;
     font-size: 2.5em;
